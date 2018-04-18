3f52372a56ab03055ace08bb649fa51501ac6059
Harvey <harvey@hcdesigns.nl>
2018-04-18 17:13:42 +0200

    Add secret + default vcl support + update default env

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
-varnishd -s malloc,${VARNISH_MEMORY} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}
+varnishd -s malloc,${VARNISH_MEMORY} \
+    -T :6082 -a :80 \
+    -p feature=+esi_ignore_other_elements -p vcc_allow_inline_c=on -p cli_buffer=16384 \
+    -S /etc/varnish/secret \
+    -f /etc/varnish/default.vcl
 sleep 1
 varnishlog
